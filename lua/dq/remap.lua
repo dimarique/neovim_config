376df27aa5f7204d2e6c5a2e22b5eaa8b53b320e
dimarique <pochtagreen@gmail.com>
2025-03-31 10:44:04 +0200

    add shortcuts for switching between splits

--- lua/dq/remap.lua
+++ lua/dq/remap.lua
@@ -9,3 +9,10 @@ vim.keymap.set('n', '<PageDown>', ':bp<CR>')
 vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { silent = true })
 vim.keymap.set('n', 'ss', ':noh<CR>')
 vim.keymap.set("n", "<space>fn", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
+
+
+vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true, silent = true })
