a41f30eae4ab82f1aa85ae0c3db2b5e29739732a
dimarique <pochtagreen@gmail.com>
2025-04-11 20:57:43 +0200

    add some more commands to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,65 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
+
+-- 🔍 Поиск файлов
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+
+-- 🔍 Поиск по содержимому
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
+
+-- 📄 Открытые буферы
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+
+-- 🧠 Определения символов
+vim.keymap.set('n', '<leader>fd', function()
+	builtin.lsp_definitions({
+		show_line = false,
+		layout_strategy = 'vertical',
+		layout_config = {
+			preview_height = 0.6,
+		},
+	})
+end, { desc = 'LSP Definitions (Telescope)' })
+
+-- 🧠 Переход к определению (альтернатива gd)
+vim.keymap.set('n', 'gd', function()
+	builtin.lsp_definitions({
+		show_line = false,
+		layout_strategy = 'vertical',
+		layout_config = {
+			preview_height = 0.6,
+		},
+	})
+end, { desc = 'Go to Definition (Telescope)' })
+
+-- 🧠 Переход к объявлению
+vim.keymap.set('n', 'gD', function()
+	builtin.lsp_type_definitions({
+		layout_strategy = 'vertical',
+		layout_config = {
+			preview_height = 0.6,
+		},
+	})
+end, { desc = 'Go to Type Definition (Telescope)' })
+
+-- 🧠 Переход к реализации
+vim.keymap.set('n', 'gi', function()
+	builtin.lsp_implementations({
+		layout_strategy = 'vertical',
+		layout_config = {
+			preview_height = 0.6,
+		},
+	})
+end, { desc = 'Go to Implementation (Telescope)' })
+
+-- 🧠 Найти ссылки на символ
+vim.keymap.set('n', 'gr', function()
+	builtin.lsp_references({
+		layout_strategy = 'vertical',
+		layout_config = {
+			preview_height = 0.6,
+		},
+	})
+end, { desc = 'Find References (Telescope)' })
+
+-- 🧠 Документация по символу
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Document Symbols' })
