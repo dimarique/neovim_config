490ecc848e4ce8f02e5f614a02f7c04b6b30ce7f
dimarique <pochtagreen@gmail.com>
2025-05-27 20:36:35 +0200

    add css icon setting to devicons plugin

--- after/plugin/devicons.lua
+++ after/plugin/devicons.lua
@@ -1,10 +1,15 @@
 require('nvim-web-devicons').setup {
-	override = {
-		html = {
-			icon = "", -- Задай свою иконку
-			color = "#E34C26", -- Цвет
-			name = "html",
-		},
-	},
-	default = true, -- Использовать иконки по умолчанию, если расширение не настроено
+  override = {
+    html = {
+      icon = "", -- Задай свою иконку
+      color = "#E34C26", -- Цвет
+      name = "html",
+    },
+    css = {
+      icon = "", -- Это флаг CSS3
+      color = "#264de4",
+      name = "css",
+    },
+  },
+  default = true, -- Использовать иконки по умолчанию, если расширение не настроено
 }
