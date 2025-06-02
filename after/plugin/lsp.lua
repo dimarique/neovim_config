a7caa9070377ba4bf4c3827bab7a98d6781c2822
dimarique <pochtagreen@gmail.com>
2025-06-02 19:47:46 +0200

    add React support in lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,65 +8,55 @@ lsp_zero.extend_cmp()
 
 
 cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<Tab>'] = cmp.mapping.select_next_item(),
-		['<S-Tab>'] = cmp.mapping.select_prev_item(),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer', keyword_length = 5 },
-	}),
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer', keyword_length = 5 },
+  }),
 })
---cmp.setup({
---completion = {
---keyword_length = 2,
---},
---sources = cmp.config.sources({
---{ name = 'nvim_lsp' },
---{ name = 'luasnip' },
---}, {
---{ name = 'buffer', keyword_length = 5 },
---}),
---})
 
 -- Настройка keymaps при подключении LSP
 lsp_zero.on_attach(function(client, bufnr)
-	lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- Настройка конкретного сервера (lua_ls)
 require('lspconfig').lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 })
 
 -- mason + mason-lspconfig
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	handlers = {
-		lsp_zero.default_setup,
-	},
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
 
 -- Показ диагностики при удержании курсора
 vim.api.nvim_create_autocmd("CursorHold", {
-	pattern = "*",
-	callback = function()
-		vim.diagnostic.open_float(nil, { focusable = false })
-	end,
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
 })
+require('lspconfig').ts_ls.setup({})
