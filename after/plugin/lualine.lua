36452cbd2adb9fe93e1a0b3eec7cdbd23c678400
dimarique <pochtagreen@gmail.com>
2025-06-15 12:16:04 +0200

    remove tabs indicator in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,68 +1,46 @@
---local function buffer_word_and_char_count()
---local wc = vim.fn.wordcount()
---return string.format("%d W. %d Z.", wc.words, wc.chars)
---end
-
 require('lualine').setup({
-	options = {
-		theme = 'auto',
-		section_separators = '',
-		component_separators = '',
-		disabled_filetypes = { 'NvimTree', 'packer', 'startify' },
-		refresh = { statusline = 1000 },
-	},
-	sections = {
-		lualine_a = { 'mode' },
+  options = {
+    theme = 'auto',
+    section_separators = '',
+    component_separators = '',
+    disabled_filetypes = { 'NvimTree', 'packer', 'startify' },
+    refresh = { statusline = 1000 },
+  },
+  sections = {
+    lualine_a = { 'mode' },
 
-		lualine_b = {
-			{
-				'branch',
-				color = function()
-					local branch = vim.fn.systemlist('git rev-parse --abbrev-ref HEAD')[1]
-					if branch == 'main' or branch == 'master' then
-						return { bg = '#FF0000' } -- Красный цвет
-					end
-				end
-			},
-			'diff'
-		},
-		lualine_c = { 'diagnostics' },
-		lualine_x = {
-			{
-				'filename',
-				path = 3,
-			}
-		},
-		lualine_y = {},
-		--lualine_z = {
-		--buffer_word_and_char_count, function() return os.date('%H:%M:%S') end
-		--},
-	},
-	tabline = {
-		lualine_a = {
-			{
-				'buffers',
-				max_length = vim.o.columns
-			}
-		},
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {
-			function()
-				local tabs = {}
-				local current = vim.fn.tabpagenr()
-				local total = vim.fn.tabpagenr('$')
-				for i = 1, total do
-					if i == current then
-						table.insert(tabs, '[' .. i .. ']')
-					else
-						table.insert(tabs, tostring(i))
-					end
-				end
-				return 'Tabs: ' .. table.concat(tabs, ' ')
-			end
-		},
-	},
+    lualine_b = {
+      {
+        'branch',
+        color = function()
+          local branch = vim.fn.systemlist('git rev-parse --abbrev-ref HEAD')[1]
+          if branch == 'main' or branch == 'master' then
+            return { bg = '#FF0000' } -- Красный цвет
+          end
+        end
+      },
+      'diff'
+    },
+    lualine_c = { 'diagnostics' },
+    lualine_x = {
+      {
+        'filename',
+        path = 3,
+      }
+    },
+    lualine_y = {},
+  },
+  tabline = {
+    lualine_a = {
+      {
+        'buffers',
+        max_length = vim.o.columns
+      }
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
 })
